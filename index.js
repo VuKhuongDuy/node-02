6ac642fa9178ecf03e3f15e2a295ad512d5b535f
VukhuongDuy <vukhuongduy23@gmail.com>
2019-04-18 20:08:54 +0700

    fasd

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 const http = require('http');
 const url = require('url');
 
-let host = process.env.HOST || "0.0.0.0";
+let host = process.env.HOST || "localhost";
 let port = process.env.PORT || 3000;
 
 var server = http.createServer(function (req, res) {
     res.writeHead(200, { "Content-Type": "text/plain" });
-    if (req.url == '/hello' && req.method === "GET")
+    console.log(req.method);
+    console.log(req.url);
+    if (req.url == '/hello' && req.method == "GET")
         res.write('world')
-    else if (req.url == '/hello' && req.method === 'POST')
+    else if (req.url == '/hello' && req.method == 'POST')
         res.write('world created')
-    else if (req.url == '/hello' && req.method === 'PUT')
+    else if (req.url == '/hello' && req.method == 'PUT')
         res.write('world updated')
-    else if (req.url == '/hello' && req.method === 'DELETE')
+    else if (req.url == '/hello' && req.method == 'DELETE')
         res.write('world deleted')
     res.end();
 });
